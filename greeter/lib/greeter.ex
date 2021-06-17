91e6981878203c3e2aaf43a59331f7de5f2a196e
Hamza Tek <hamza@iboard.cc>
2021-06-17 17:59:18 +0200

    Add greeting with age

--- greeter/lib/greeter.ex
+++ greeter/lib/greeter.ex
@@ -1,5 +1,8 @@
 defmodule Greeter do
   def hello(name \\ "World") 
+  def hello(%{name: name, age: age}) do
+    "My name is #{name}. I'm #{age}."
+  end
   def hello(%{name: name}) do
     "Hello, #{name}!"
   end
@@ -8,3 +11,4 @@ defmodule Greeter do
     "Hello, #{name}!"
   end
 end
+
--- greeter/test/greeter_test.exs
+++ greeter/test/greeter_test.exs
@@ -15,6 +15,12 @@ defmodule GreeterTest do
     alice = %{name: "Alice"}
     assert Greeter.hello(bob) == "Hello, Bob!"
     assert Greeter.hello(alice) == "Hello, Alice!"
-
   end
+
+  test "person has an age" do
+   hamza = %{age: 32, name: "Hamza"}
+   andi = %{age: 57, name: "Andi"}
+   assert "My name is Hamza. I'm 32." == Greeter.hello(hamza)
+   assert "My name is Andi. I'm 57." == Greeter.hello(andi)
+   end
 end
