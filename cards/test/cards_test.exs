c02cf8b65d1c5962ba6c121f7690e4fd45a86bfe
Hamza Tek <hamza@iboard.cc>
2021-07-08 21:12:35 +0200

    Adding shuffle function

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -6,11 +6,24 @@ defmodule CardsTest do
     card = Cards.new("card1")
     assert card.header == "card1"
   end
+
   test "Cards.stack() is a list of cards" do
     a = Cards.new("Hello I am the first card!")
     b = Cards.new("Hello I am the second card!")
     c = Cards.new("Hello I am the third card!")
     stack = Cards.stack([a,b,c])
     assert stack.cards == [a,b,c]
+  end
+
+  test "draw a card" do
+    #given
+    a = Cards.new("Hello I am the first card!")
+    b = Cards.new("Hello I am the second card!")
+    c = Cards.new("Hello I am the third card!")
+    stack = Cards.stack([a,b,c])
+
+    #then
+    card = Cards.draw(stack)
+    assert Enum.member?(stack.cards, card)
   end
 end
