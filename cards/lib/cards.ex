46956251f9d20176ee95f607246b7105d0010b4f
Annalisa Wiesinger <anna.wiesinger@outlook.at>
2021-07-08 18:48:15 +0200

    make the first test green

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,18 +1,6 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def new do
+    %{question: nil}
   end
 end
