abe1dfb9afab1e50f64ecdd9f6dc6a4eaa2b4606
Annalisa Wiesinger <anna.wiesinger@outlook.at>
2021-07-08 19:28:48 +0200

    add test for stack

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -6,4 +6,11 @@ defmodule CardsTest do
     card = Cards.new("card1")
     assert card.header == "card1"
   end
+  test "Cards.stack() is a list of cards" do
+    a = Cards.new("Hello I am the first card!")
+    b = Cards.new("Hello I am the second card!")
+    c = Cards.new("Hello I am the third card!")
+    stack = Cards.stack([a,b,c])
+    assert stack.cards == [a,b,c]
+  end
 end
