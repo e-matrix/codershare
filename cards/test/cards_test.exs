f31407ffee28284d7d2be20f688e0d3cebfe419e
Hamza Tek <hamza@iboard.cc>
2021-07-08 18:40:03 +0200

    first test

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -2,7 +2,8 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "a card as question" do
+    card = Cards.new()
+    assert Cards.question == nil
   end
 end
